63d35923c1df4de7a64c5a1d6a6089166be6de5b
soichi <soujiro0725@gmail.com>
2018-05-01 11:29:27 +0900

    net-tools added

--- linux/install-packages.sh
+++ linux/install-packages.sh
@@ -3,4 +3,4 @@ sudo apt-get install -y curl vim ssh wget tmux
 # to install python with pyenv
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
     libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-    xz-utils tk-dev
+    xz-utils tk-dev net-tools
