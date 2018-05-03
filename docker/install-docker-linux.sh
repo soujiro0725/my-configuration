92031550d55d9639a5cb939370a5a9464cc32a96
soichi <soujiro0725@gmail.com>
2018-05-04 06:41:51 +0900

    docker installtion modified

--- docker/install-docker-linux.sh
+++ docker/install-docker-linux.sh
@@ -22,3 +22,8 @@ sudo apt-get install docker-ce
 # make sure the version is latest
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# after installation
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
