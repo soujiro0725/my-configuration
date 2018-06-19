97f081738786a24d85ed4ab56b67ef6c70e20931
soichi <soujiro0725@gmail.com>
2018-06-20 07:50:14 +0900

    added themes for ubuntu

---
+++ linux/install-themes.sh
@@ -0,0 +1,14 @@
+sudo apt install -y unity-tweak-tool
+sudo apt-get install notify-osd
+
+# add daily builds PPA
+sudo add-apt-repository -y ppa:snwh/pulp
+# update repository info
+sudo apt-get update
+# install icon theme
+sudo apt-get install paper-icon-theme -y
+# install gtk theme
+sudo apt-get install paper-gtk-theme -y
+# install cursor theme
+sudo apt-get install paper-cursor-theme -y
+
