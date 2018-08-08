cf6e32a83a82a192552f7c8500800c647f28a4bd
soujiro0725 <soujiro0725@gmail.com>
2018-08-08 17:25:21 +0900

    added tools for qemu

--- linux/install-venv.sh
+++ linux/install-venv.sh
@@ -1,3 +1,8 @@
 sudo apt-get install gdb-multiarch
 sudo apt-get install g++-arm-linux-gnueabihf
 sudo apt-get install qemu-user-binfmt
+
+# reference
+# https://qiita.com/eggman/items/948095368ae81834fb4a
+sudo apt build-dep qemu
+sudo apt install gcc-arm-linux-gnueabihf
