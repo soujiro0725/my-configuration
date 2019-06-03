37edf7a3599f5189796b7d4a7ad47a799d470e1a
Soichi Ishida <sishida@gsx.co.jp>
2019-06-04 07:19:59 +0900

    docker-compose updated

--- docker/install-docker-linux.sh
+++ docker/install-docker-linux.sh
@@ -23,7 +23,7 @@ sudo apt-get install docker-ce
 
 # install docker-compose
 # make sure the version is latest
-sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # after installation
