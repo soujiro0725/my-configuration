a0352259cc42dee1bf076a461bcdbafba23c81d0
soujiro0725 <soujiro0725@gmail.com>
2019-11-19 19:22:02 +0900

    minor change

--- pwn/docker/run_container.sh
+++ pwn/docker/run_container.sh
@@ -1,1 +1,1 @@
-docker run --rm -v $PWD:/pwd --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -p 5555:5555 -i ubuntu18:ctf
+docker run --rm -v $PWD:/pwd --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -p 5555:5555 -i ubuntu18:ctf  
