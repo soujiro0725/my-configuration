47992b34c1bb06be4fc9baf6329c0b61fc20da9a
soujiro0725 <soujiro0725@gmail.com>
2019-12-01 09:12:55 +0900

    scala env added

--- emacs/init.el
+++ emacs/init.el
@@ -460,6 +460,29 @@
   :custom (ccls-executable "/usr/local/bin/ccls")
   :hook ((c-mode c++-mode) .
          (lambda () (require 'ccls) (lsp))))
+
+;; for Scala
+;; Enable scala-mode and sbt-mode
+(use-package scala-mode
+  :mode "\\.s\\(cala\\|bt\\)$")
+
+(use-package sbt-mode
+  :commands sbt-start sbt-command
+  :config
+  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
+  ;; allows using SPACE when in the minibuffer
+  (substitute-key-definition
+   'minibuffer-complete-word
+   'self-insert-command
+   minibuffer-local-completion-map)
+   ;; sbt-supershell kills sbt-mode:  https://github.com/hvesalai/emacs-sbt-mode/issues/152
+   (setq sbt:program-options '("-Dsbt.supershell=false"))
+   )
+
+(use-package scala-mode
+  :config
+  (add-hook 'scala-mode-hook #'lsp))
+
 ;;--------------------------------------------------
 
 
