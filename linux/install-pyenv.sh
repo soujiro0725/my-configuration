9f7c7aee92272cbb13203f0605579f533260100e
Ubuntu <ubuntu@ip-10-0-0-129.ap-northeast-1.compute.internal>
2023-04-08 01:07:44 +0000

    updated pyenv installation process

--- linux/install-pyenv.sh
+++ linux/install-pyenv.sh
@@ -2,12 +2,12 @@
 
 sudo apt-get update
 sudo apt-get install git vim
-git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 echo 'export PYENV_ROOT=$HOME/.pyenv' >> ~/.bashrc
 echo 'export PATH=$PYENV_ROOT/bin:$PATH' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
 echo 'installing pyenv-update...'
-git clone https://github.com/yyuu/pyenv-update.git ~/.pyenv/plugins/pyenv-update
+git clone https://github.com/pyenv/pyenv-update.git ~/.pyenv/plugins/pyenv-update
 
